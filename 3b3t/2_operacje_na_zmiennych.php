813bf30ef1928f58c43fd5762de1923aea52fcb9
mplewa <70716924+mplewa@users.noreply.github.com>
2020-09-17 19:52:42 +0200

    2_operacje_na_zmiennych.php

--- 3b3t/2_operacje_na_zmiennych.php
+++ 3b3t/2_operacje_na_zmiennych.php
@@ -26,6 +26,72 @@
     $y = 20;
     echo $x <=> $y;
     $result = $x <=> $y;
-    echo $result;
+    echo "$result<br>";
+
+    $x=1;
+    $y=1.0;
+
+    if ($x==$y)
+    {
+      echo 'Zmienne są równe<br>';
+    }
+      else
+      {
+        echo 'Zmienne są różne<br>';
+      }
+echo gettype($x), '<br>'; //integer
+echo gettype($y), '<br>'; //double
+
+####################################################################
+
+/*
+preinkrementacja ++$x
+predekrementacja --$x
+postinkrementacja $x++
+postdekrementacja $x--
+*/
+
+$x=1;
+$x=$x++;
+echo $x, '<br>'; //1
+
+$x=++$x;
+echo $x, '<br>'; //2
+
+$y=$x++;
+echo $x, '<br>'; //3
+echo $y, '<br>'; //2
+
+$y=++$x*2-1;
+echo $x, '<br>'; //4
+echo $y, '<br>'; //7
+
+$x=2;
+echo $x++; //2
+echo ++$x; //4
+echo $x; //4
+
+$y=$x++;
+echo $y; //4
+
+$y=++$x;
+echo $y; //6
+echo ++$y; //7
+
+//operatory rzutowania
+//bool, int, float, string, array, object, unset
+
+$test='123abc';
+$test1=0;
+$test2=20;
+
+echo '<hr>Typ danych $test: ', gettype($test);
+$x=(int)$test;
+echo "<hr>$x<br>";
+echo 'Typ danych $x: ', gettype($x);
+
+$x=(bool)$test1;
+echo "<hr>$x<br>";
+echo 'Typ danych $x: ', gettype($x);
 
 ?>
